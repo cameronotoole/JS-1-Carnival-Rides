e564ea703202020ddca5476b38151eada477dcc6
cameron otoole <cameronotoole72@gmail.com>
2024-02-29 02:06:42 -0500

    fixed rides 1-3, started fixing ride4 not complete

--- index.js
+++ index.js
@@ -10,30 +10,51 @@ let waterIsOn = true
 //Everything below is fair game for you to power on the rides
 
 //Ride 1 works when:
-    // green wire is connected
+// green wire is connected
 ride1PowerOn = false
 
+if (greenWireConnected) {
+    ride1PowerOn = true
+}
+
+
 //Ride 2 works when 
-    //green and yellow wires are connected
+//green and yellow wires are connected
 ride2PowerOn = false
 
+if (greenWireConnected && yellowWireConnected) {
+    ride2PowerOn = true
+}
+
 //Ride 3 works when:
-    // green and red wires are connected
-    // water is on 
-    // yellow wire is not connected
+// green and red wires are connected
+// water is on 
+// yellow wire is not connected
 ride3PowerOn = false
 
+//  this is the only way to make this make sense i think
+yellowWireConnected = false;
+
+if (greenWireConnected && redWireConnected && waterIsOn && !yellowWireConnected) {
+    ride3PowerOn = true
+}
+console.log(redWireConnected)
 //Ride 4 has power when:
-    //ride wire is connected
+//ride wire is connected
 ride4PowerOn = false
+
+if (redWireConnected) {
+    ride4PowerOn = true;
+}
+
 //Ride 4 is a slot machine
-    //if a 1 is rolled it should return "star"
-    //if a 2 is rolled it should return "car"
-    //if a 3 is rolled it should return "pot of gold"
-    //if a 4 is rolled it should return "clover"
-    //if a 5 is rolled it should return "bowling ball"
-    //if a 6 is rolled it should return "tiger"
-    //if a 7 is rolled it should return "seven"
-function ride4SlotMachine(rolledNumber){
+//if a 1 is rolled it should return "star"
+//if a 2 is rolled it should return "car"
+//if a 3 is rolled it should return "pot of gold"
+//if a 4 is rolled it should return "clover"
+//if a 5 is rolled it should return "bowling ball"
+//if a 6 is rolled it should return "tiger"
+//if a 7 is rolled it should return "seven"
+function ride4SlotMachine(rolledNumber) {
     return "nothing"
 }
